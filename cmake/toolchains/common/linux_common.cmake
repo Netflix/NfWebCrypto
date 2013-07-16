ff60090a08d8ec6b3d330665e8b57b056b361c3f
Paul Adolph <padolph@netflix.com>
2013-07-15 23:52:47 -0700

    Update linux_common.cmake with a note about changing the SECRET_SYSTEM_KEY.

--- cmake/toolchains/common/linux_common.cmake
+++ cmake/toolchains/common/linux_common.cmake
@@ -19,6 +19,7 @@
 get_filename_component(TOOLCHAIN_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
 include("${TOOLCHAIN_DIR}/common.cmake")
 
+# NOTE: SECRET_SYSTEM_KEY should be changed for actual deployments.
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DSECRET_SYSTEM_KEY=iIniW9SVpZKlXGmbrgrJG9uxy7HtCNJsDM5IXS24eCI=")
 
 # Toolchain paths - for desktop linux we just use the system tools
