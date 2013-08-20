6e6aaccfccaf83679e8231ebd267fd867d6f5592
Paul Adolph <padolph@netflix.com>
2013-08-20 11:32:58 -0700

    Fixed memory leak in AES CBC cipher.

--- crypto/AesCbcCipher.cpp
+++ crypto/AesCbcCipher.cpp
@@ -20,6 +20,7 @@
 #include <assert.h>
 #include <openssl/bn.h>
 #include <openssl/evp.h>
+#include <crypto/ScopedOpenSSL.h>
 
 namespace cadmium {
 namespace crypto {
@@ -49,14 +50,14 @@ bool AesCbcCipher::encrypt(const Vuc& in, Vuc& out)
     out.resize(outSize, 0);
 
     // init encryption
-    EVP_CIPHER_CTX ctx;
-    EVP_EncryptInit(&ctx, getCipher(), &key_[0], &iv_[0]);
+    ScopedOpenSSL<EVP_CIPHER_CTX, EVP_CIPHER_CTX_free> ctx(EVP_CIPHER_CTX_new());
+    EVP_EncryptInit(ctx.get(), getCipher(), &key_[0], &iv_[0]);
 
     // do the encrypt; must keep track of the number of bytes produced
     int nBytes = 0;
-    EVP_EncryptUpdate(&ctx, &out[0], &nBytes, &in[0], in.size());
+    EVP_EncryptUpdate(ctx.get(), &out[0], &nBytes, &in[0], in.size());
     int nTotalBytes = nBytes;
-    EVP_EncryptFinal(&ctx, &out[nBytes], &nBytes);
+    EVP_EncryptFinal(ctx.get(), &out[nBytes], &nBytes);
     nTotalBytes += nBytes;
 
     // the actual output size is in nTotalBytes
@@ -76,14 +77,14 @@ bool AesCbcCipher::decrypt(const Vuc& in, Vuc& out)
     out.resize(outSize, 0);
 
     // init the cipher; must keep track of the number of bytes produced
-    EVP_CIPHER_CTX ctx;
-    EVP_DecryptInit(&ctx, getCipher(), &key_[0], &iv_[0]);
+    ScopedOpenSSL<EVP_CIPHER_CTX, EVP_CIPHER_CTX_free> ctx(EVP_CIPHER_CTX_new());
+    EVP_DecryptInit(ctx.get(), getCipher(), &key_[0], &iv_[0]);
 
     // do the decrypt
     int nBytes = 0;
-    EVP_DecryptUpdate(&ctx, &out[0], &nBytes, &in[0], in.size());
+    EVP_DecryptUpdate(ctx.get(), &out[0], &nBytes, &in[0], in.size());
     int nTotalBytes = nBytes;
-    if (!EVP_DecryptFinal(&ctx, &out[nBytes], &nBytes))
+    if (!EVP_DecryptFinal(ctx.get(), &out[nBytes], &nBytes))
         return false;   // padding incorrect
     nTotalBytes += nBytes;
 
