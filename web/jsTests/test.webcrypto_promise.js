20cadec94691e8d90cc0f1e1945c5ffa7a8bffb4
Paul Adolph <padolph@netflix.com>
2015-03-23 14:38:33 -0700

    testing increasing the RSA key size to make the 'RSA-OAEP wrap/unwrap JWK round trip' test pass

--- web/jsTests/test.webcrypto_promise.js
+++ web/jsTests/test.webcrypto_promise.js
@@ -1903,7 +1903,7 @@
                 cryptoSubtle.generateKey(
                     {
                         name: "RSA-OAEP",
-                        modulusLength: 1024,
+                        modulusLength: 2048,
                         publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
                         hash: {name: "SHA-1"}
                     },
